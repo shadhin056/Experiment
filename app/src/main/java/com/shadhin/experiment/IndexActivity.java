ce627e6aa5df40d902cd13972598d68b10fe98e3
ShadhiN <mac@macs-MacBook-Pro.local>
2020-12-04 21:52:12 +0600

    button click voice to text

--- app/src/main/java/com/shadhin/experiment/IndexActivity.java
+++ app/src/main/java/com/shadhin/experiment/IndexActivity.java
@@ -9,17 +9,20 @@ import android.widget.Button;
 
 import com.shadhin.experiment.custom_camera.CustomCamera2Activity;
 import com.shadhin.experiment.custom_camera.CustomCameraActivity;
+import com.shadhin.experiment.voice.VoiceCommandActivity;
 
 public class IndexActivity extends AppCompatActivity {
 
     Button btnBlink;
     Button btnCustomCamera;
+    Button btnVoice;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_index);
         btnBlink=findViewById(R.id.btnBlink);
         btnCustomCamera=findViewById(R.id.btnCustomCamera);
+        btnVoice=findViewById(R.id.btnVoice);
         btnBlink.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -32,6 +35,12 @@ public class IndexActivity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 Intent intent=new Intent(IndexActivity.this, CustomCameraActivity.class);
+                startActivity(intent);
+            }
+        }); btnVoice.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent=new Intent(IndexActivity.this, VoiceCommandActivity.class);
                 startActivity(intent);
             }
         });
